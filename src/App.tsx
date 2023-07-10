5ba506c32521aad9ba93fd25ca2df28d79b9c7b5
Zhongren Shao <shao.zhongren@gmail.com>
2023-07-10 01:52:52 -0500

    helloPoint1 with separate files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,102 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
+import helloPoint1Frag from "./shaders/helloPoint1/fragmentShader.frag";
+import helloPoint1Vert from "./shaders/helloPoint1/vertexShader.vert";
+
+function createShader(
+  gl: WebGLRenderingContext,
+  type: number,
+  source: string
+): WebGLShader | null {
+  const shader = gl.createShader(type);
+  if (!shader) {
+    console.error("An error occurred creating the shaders");
+    return null;
+  }
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(
+      "An error occurred compiling the shaders:",
+      gl.getShaderInfoLog(shader)
+    );
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+function createProgram(
+  gl: WebGLRenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader
+): WebGLProgram | null {
+  const program = gl.createProgram();
+  if (!program) {
+    console.error("An error occurred creating the program");
+    return null;
+  }
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(
+      "Unable to initialize the shader program:",
+      gl.getProgramInfoLog(program)
+    );
+    gl.deleteProgram(program);
+    return null;
+  }
+
+  return program;
+}
+
+async function initShaders(
+  gl: WebGLRenderingContext,
+  vertexShaderSource: string,
+  fragmentShaderSource: string
+): Promise<WebGLProgram | null> {
+  const vertexShader = createShader(
+    gl,
+    gl.VERTEX_SHADER,
+    vertexShaderSource
+  ) as WebGLShader;
+  const fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource
+  ) as WebGLShader;
+
+  const program = createProgram(gl, vertexShader, fragmentShader);
+
+  // Set up shader program here
+
+  return program;
+}
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const gl = canvas?.getContext("webgl2");
+    const loadShadersAndDraw = async () => {
+      const canvas = canvasRef.current;
+      const gl = canvas?.getContext("webgl2");
 
-    if (!gl) {
-      return;
-    }
+      if (!gl) {
+        return;
+      }
 
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+      const program = await initShaders(gl, helloPoint1Vert, helloPoint1Frag);
+
+      gl.clearColor(0.0, 0.0, 0.0, 1.0);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.useProgram(program);
+      gl.drawArrays(gl.POINTS, 0, 1);
+    };
+    loadShadersAndDraw();
   }, []);
   return <canvas ref={canvasRef} width="800" height="1000"></canvas>;
 }
