7234a528832075c5e98c8ac54f603e878c240d29
Zhongren Shao <shao.zhongren@gmail.com>
2023-07-28 19:09:26 -0500

    texturedQuadClampMirror

--- src/helpers/initTextures.tsx
+++ src/helpers/initTextures.tsx
@@ -30,15 +30,18 @@ function loadTexture(
   u_Sampler: WebGLUniformLocation | null,
   image: HTMLImageElement
 ) {
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+  // Flip the image's y axis because WebGL coordinate system is different from the image's coordinate system, webgl's origin is bottom left, image's origin is top left
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
   // Enable texture unit0
   gl.activeTexture(gl.TEXTURE0);
-  // Bind the texture object to the target
+  // Bind the texture object to the target, with the type of texture, and also binds it to the texture unit above
   gl.bindTexture(gl.TEXTURE_2D, texture);
 
-  // set texture parameters
+  // set texture parameters, set param for minification filter to LINEAR, so that when the texture is minified, it will use LINEAR filter to interpolate the texels
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  // set texture image
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+  // assign texture image to texture object, with image internal format as RGB, and texel data as RGB, and the data type as UNSIGNED_BYTE. 0 is because we are not using a MIPMAP texture
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
 
   // set the texture unit 0 to the sampler
