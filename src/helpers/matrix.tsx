bbd4132257498f188217d5b82bed573550631898
Zhongren Shao <shao.zhongren@gmail.com>
2023-09-14 23:51:11 -0500

    fix addTranslate and addScale to match cuon-matrix

--- src/helpers/matrix.tsx
+++ src/helpers/matrix.tsx
@@ -1,4 +1,3 @@
-var logged = 0;
 class TransformMatrix4 {
   elements: Float32Array;
 
@@ -55,8 +54,11 @@ class TransformMatrix4 {
   }
 
   addTranslate(x: number, y: number, z: number) {
-    var translationMatrix = new TransformMatrix4().setTranslate(x, y, z);
-    this.elements = translationMatrix.multiply(this).elements;
+    var e = this.elements;
+    e[12] += e[0] * x + e[4] * y + e[8] * z;
+    e[13] += e[1] * x + e[5] * y + e[9] * z;
+    e[14] += e[2] * x + e[6] * y + e[10] * z;
+    e[15] += e[3] * x + e[7] * y + e[11] * z;
     return this;
   }
 
@@ -214,8 +216,19 @@ class TransformMatrix4 {
   }
 
   addScale(x: number, y: number, z: number) {
-    var scaleMatrix = new TransformMatrix4().setScale(x, y, z);
-    this.elements = scaleMatrix.multiply(this).elements;
+    var e = this.elements;
+    e[0] *= x;
+    e[1] *= x;
+    e[10] *= z;
+    e[11] *= z;
+    e[2] *= x;
+    e[3] *= x;
+    e[4] *= y;
+    e[5] *= y;
+    e[6] *= y;
+    e[7] *= y;
+    e[8] *= z;
+    e[9] *= z;
     return this;
   }
 
